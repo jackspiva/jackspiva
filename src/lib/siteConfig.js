2a552b667ff5d1269d6ef649cc3e647beee8ff0a
Jack Spiva <johnspiva@gmail.com>
2022-02-21 19:01:16 -0600

    Fix twitter handle for real

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -4,7 +4,7 @@ export const REPO_URL = 'https://github.com/' + GH_USER_REPO
 export const SITE_TITLE = 'Jack Spiva'
 export const SITE_DESCRIPTION = "Jack Spiva's personal website"
 export const DEFAULT_OG_IMAGE = ''
-export const MY_TWITTER_HANDLE = 'jackspiva'
+export const MY_TWITTER_HANDLE = 'jack_spiva'
 
 // dont forget process.env.GH_TOKEN
 // if supplied, raises rate limit from 60 to 5000
